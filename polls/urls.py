647492df4fccf1691d3144638e56463a9fea2ce4
triple <wog20051221@gmail.com>
2025-06-05 01:09:33 +0200

    Delete

--- polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Это для главной страницы /polls/
-    path('question/<int:question_id>/', views.detail, name='detail'),
-    path('question/<int:question_id>/results/', views.results, name='results'),
-    path('question/<int:question_id>/vote/', views.vote, name='vote'),
-]
